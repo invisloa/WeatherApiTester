c8ef5636af3198eff7279d3c613aaba4f67a86cf
invisloa <91217120+invisloa@users.noreply.github.com>
2023-06-08 23:43:11 +0200

    WeatherServiceTommorowIO refactor

--- WeatherApiTester/Services/WeatherServices/WeatherServiceTommorowIO.cs
+++ WeatherApiTester/Services/WeatherServices/WeatherServiceTommorowIO.cs
@@ -10,106 +10,54 @@ using WeatherApiTester.Model.WeatherApiModels.TommorowIO;
 
 namespace WeatherApiTester.Services.WeatherServices
 {
-    public class WeatherServiceTommorowIO : IGetWeatherData
+	public class WeatherServiceTommorowIO : IGetWeatherData
 	{
 		private static readonly HttpClient client = new HttpClient();
-		string _myApiKey = "Cj66O8OLTih8hPqA7AOKfevJuX11N1hp";
-		string _location = "Warszawa";
+		private const string _myApiKey = "Cj66O8OLTih8hPqA7AOKfevJuX11N1hp";
+		private const string _location = "Warszawa";
 
-		public async Task<IWeatherModel> GetWeatherCurrentAsync()
+		static WeatherServiceTommorowIO()
 		{
 			client.DefaultRequestHeaders.Accept.Clear();
 			client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-			try
-			{
-				var uri = new Uri($"https://api.tomorrow.io/v4/weather/realtime?location={_location}&apikey={_myApiKey}");
-
-				HttpResponseMessage response = await client.GetAsync(uri);
-				if (response.IsSuccessStatusCode)
-				{
-					string responseBody = await response.Content.ReadAsStringAsync();
-
-					IWeatherModel myDeserializedClass = JsonConvert.DeserializeObject<WeatherModelTommorowIOCurrent>(responseBody);
-
-					return myDeserializedClass;
-				}
-				else
-				{
-					throw new Exception($"Error: {response.StatusCode}");
-				}
-			}
-			catch (Exception ex)
-			{
-				throw new Exception($"Exception: {ex.Message}");
-			}
 		}
 
-		public async Task<IWeatherModel> GetWeatherForecastDailyAsync()
+		private Uri CreateUri(string endpoint, string parameters = "")
 		{
-			client.DefaultRequestHeaders.Accept.Clear();
-			client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-			try
-			{
-				var uri = new Uri($"https://api.tomorrow.io/v4/weather/forecast?location={_location}&timesteps=1d&apikey={_myApiKey}");
-
-				HttpResponseMessage response = await client.GetAsync(uri);
-				if (response.IsSuccessStatusCode)
-				{
-					string responseBody = await response.Content.ReadAsStringAsync();
-
-					IWeatherModel myDeserializedClass = JsonConvert.DeserializeObject<WeatherModelTommorowIODailyForecast>(responseBody);
-
-					return myDeserializedClass;
-				}
-				else
-				{
-					throw new Exception($"Error: {response.StatusCode}");
-				}
-			}
-			catch (Exception ex)
-			{
-				throw new Exception($"Exception: {ex.Message}");
-			}
-
+			return new Uri($"https://api.tomorrow.io/v4/weather/{endpoint}?location={_location}&apikey={_myApiKey}{parameters}");
 		}
 
-		public async Task<IWeatherModel> GetWeatherHistoricAsync()
+		private async Task<IWeatherModel> SendRequestAsync<T>(Uri uri)
+		{
+			HttpResponseMessage response = await client.GetAsync(uri);
+
+			if (!response.IsSuccessStatusCode)
+			{
+				throw new Exception($"Error: {response.StatusCode}");
+			}
+
+			string responseBody = await response.Content.ReadAsStringAsync();
+			return JsonConvert.DeserializeObject<WeatherModelTommorowIODailyForecast>(responseBody);
+		}
+
+		public Task<IWeatherModel> GetWeatherCurrentAsync()
+		{
+			return SendRequestAsync<WeatherModelTommorowIOCurrent>(CreateUri("realtime"));
+		}
+
+		public Task<IWeatherModel> GetWeatherForecastDailyAsync()
+		{
+			return SendRequestAsync<WeatherModelTommorowIODailyForecast>(CreateUri("forecast", "&timesteps=1d"));
+		}
+
+		public Task<IWeatherModel> GetWeatherForecastHourlyAsync()
+		{
+			return SendRequestAsync<WeatherModelTommorowIOHourlyForecast>(CreateUri("forecast", "&timesteps=1h"));
+		}
+
+		public Task<IWeatherModel> GetWeatherHistoricAsync()
 		{
 			throw new NotImplementedException();
-
 		}
-
-		public async Task<IWeatherModel> GetWeatherForecastHourlyAsync()
-		{
-			client.DefaultRequestHeaders.Accept.Clear();
-			client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-			try
-			{
-				var uri = new Uri($"https://api.tomorrow.io/v4/weather/forecast?location={_location}&timesteps=1h&apikey={_myApiKey}");
-
-				HttpResponseMessage response = await client.GetAsync(uri);
-				if (response.IsSuccessStatusCode)
-				{
-					string responseBody = await response.Content.ReadAsStringAsync();
-
-					IWeatherModel myDeserializedClass = JsonConvert.DeserializeObject<WeatherModelTommorowIOHourlyForecast>(responseBody);
-
-					return myDeserializedClass;
-				}
-				else
-				{
-					throw new Exception($"Error: {response.StatusCode}");
-				}
-			}
-			catch (Exception ex)
-			{
-				throw new Exception($"Exception: {ex.Message}");
-			}
-		}
-
-
 	}
 }
