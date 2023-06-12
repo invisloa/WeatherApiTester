3a2e0e12072fa20c9035f22a42c2e99fa4054b45
invisloa <91217120+invisloa@users.noreply.github.com>
2023-06-13 00:00:49 +0200

    ns

--- WeatherApiTester/Model/WeatherApiModels/WeatherProperty.cs
+++ WeatherApiTester/Model/WeatherApiModels/WeatherProperty.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace WeatherApiTester.Model.WeatherApiModels
 {
-    class WeatherProperty
+    public class WeatherProperty
     {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool IsVisible { get; set; }
+        public string IconPath { get; set; }
+		public WeatherProperty(string name, string value, bool isVisible, string iconPath) 
+        {
+            Name = name;
+            Value = value;  
+            IsVisible = isVisible;
+            IconPath = iconPath;
+        }
     }
 }
--- WeatherApiTester/Services/Factory.cs
+++ WeatherApiTester/Services/Factory.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WeatherApiTester.Model.WeatherApiModels.TommorowIO;
 using WeatherApiTester.Services.WeatherServices;
 
 namespace WeatherApiTester.Services
 {
 	public static class Factory
 	{
-		public static WeatherModelTommorowIOCurrent CreateGetWeatherData => new WeatherServiceTommorowIO();
+		public static IGetWeatherData CreateGetWeatherData => new WeatherServiceTommorowIO();
 	}
 }
